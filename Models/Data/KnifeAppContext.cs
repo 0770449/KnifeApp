c89833b4011ee3226ec95b8147d95a53d071ea77
Kamaljeet Kaur <jeetkamalkaur303@gmail.com>
2022-10-16 23:30:19 -0400

    Add project files.

---
+++ Models/Data/KnifeAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KnifeApp.Models;
+
+namespace KnifeApp.Models.Data
+{
+    public class KnifeAppContext : DbContext
+    {
+        public KnifeAppContext(DbContextOptions<KnifeAppContext> options)
+           : base(options)
+    {
+    }
+
+    public DbSet<Knife> Knife { get; set; }
+}
+}
---
+++ Models/Knife.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KnifeApp.Models
+{
+    public class Knife
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        public string Type { get; set; }
+        public string Colour { get; set; }
+        public string Size { get; set; }
+        public string Image { get; set; }
+
+    }
+}
