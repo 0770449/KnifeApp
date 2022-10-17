02a11ee2cc8f4a0960ef5b30b1e7dd148be74ca8
Kamaljeet Kaur <jeetkamalkaur303@gmail.com>
2022-10-17 00:18:40 -0400

    Seed the database

---
+++ Models/SeedData.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using KnifeApp.Data;
+using System;
+using System.Linq;
+
+namespace KnifeApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new KnifeAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<KnifeAppContext>>()))
+            {
+                // Look for any knives.
+                if (context.Knife.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Knife.AddRange(
+                    new Knife
+                    {
+                        Name = "Forged",
+                        Price = 2.49M,
+                        Type = "Felt ",
+                        Colour = "Black",
+                        Size = "Small",
+                        Image = ""
+                    },
+
+
+
+                    new Knife
+                    {
+                        Name = "Stamped",
+                        Price = 2.49M,
+                        Type = "Felt ",
+                        Colour = "Black",
+                        Size = "Medium Tip",
+                        Image = ""
+                    },
+
+                     new Knife
+                     {
+                         Name = "Forged",
+                         Price = 2.49M,
+                         Type = "Felt ",
+                         Colour = "Black",
+                         Size = "Small",
+                         Image = ""
+                     },
+                    new Knife
+                    {
+                        Name = "Forged",
+                        Price = 2.49M,
+                        Type = "Felt ",
+                        Colour = "Black",
+                        Size = "Small",
+                        Image = ""
+                    },
+                    new Knife
+                    {
+                        Name = "Forged",
+                        Price = 2.49M,
+                        Type = "Felt ",
+                        Colour = "Black",
+                        Size = "Small",
+                        Image = ""
+                    },
+                     new Knife
+                     {
+                         Name = "Forged",
+                         Price = 2.49M,
+                         Type = "Felt ",
+                         Colour = "Black",
+                         Size = "Small",
+                         Image = ""
+                     },
+                    new Knife
+                    {
+                        Name = "Forged",
+                        Price = 2.49M,
+                        Type = "Felt ",
+                        Colour = "Black",
+                        Size = "Small",
+                        Image = ""
+                    },
+
+                    new Knife
+                    {
+                        Name = "Forged",
+                        Price = 2.49M,
+                        Type = "Felt ",
+                        Colour = "Black",
+                        Size = "Small",
+                        Image = ""
+                    },
+
+                    new Knife
+                    {
+                        Name = "Forged",
+                        Price = 2.49M,
+                        Type = "Felt ",
+                        Colour = "Black",
+                        Size = "Small",
+                        Image = ""
+                    },
+
+                     new Knife
+                     {
+                         Name = "Forged",
+                         Price = 2.49M,
+                         Type = "Felt ",
+                         Colour = "Black",
+                         Size = "Small",
+                         Image = ""
+                     },
+
+
+
+                     new Knife
+                     {
+                         Name = "Forged",
+                         Price = 2.49M,
+                         Type = "Felt ",
+                         Colour = "Black",
+                         Size = "Small",
+                         Image = ""
+                     },
+
+
+
+                    new Knife
+                    {
+                        Name = "Forged",
+                        Price = 2.49M,
+                        Type = "Felt ",
+                        Colour = "Black",
+                        Size = "Small",
+                        Image = ""
+                    }
+
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
