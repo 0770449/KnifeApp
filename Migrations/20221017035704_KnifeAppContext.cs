50ef6924095e28ded3a3bbaa249a75cae3705cba
Kamaljeet Kaur <jeetkamalkaur303@gmail.com>
2022-10-17 00:00:14 -0400

    Added migration and update database

---
+++ Migrations/20221017035704_KnifeAppContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KnifeApp.Migrations
+{
+    public partial class KnifeAppContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Knife",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    Colour = table.Column<string>(nullable: true),
+                    Size = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Knife", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Knife");
+        }
+    }
+}
